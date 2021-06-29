6ba9bf7bef5a0cf9fd5fc10e4f5aaaadde67bb24
Clorhexidinum <mkubekov@gmail.com>
2021-06-29 18:33:51 +0300

    g

--- app/js/main.js
+++ app/js/main.js
@@ -68,3 +68,59 @@ Vue.component("tabsCategories", {
         ],
     },
 });
+
+Vue.component("productSliderItem", {
+    template: `
+    <div class="product-slider__item">
+        <div class="product-item__wrapper">
+            <button class="product-item__favorite"></button>
+            <button class="product-item__basket">
+                <img src="images/basket-white.svg" alt=""/>
+            </button>
+            <a class="product-item__notify-link">
+                <span>Сообщить о поступлении</span>
+            </a>
+            <a class="product-item" href="#">
+                <p class="product-item__hover-text">посмотреть товар</p>
+                <img class="product-item__img" src="images/content/product-1.png" alt=""
+                />
+                <h4 class="product-item__title">
+                    Водонепроницаемый Рюкзак
+                </h4>
+                <p class="product-item__price price">9 800 ₽</p>
+                <p class="product-item__notify-text">нет в наличии</p>
+            </a>
+        </div>
+    </div>
+        `,
+    data: {
+        сategories: [
+            "запчасти",
+            "моторы",
+            "шины",
+            "электроник",
+            "инструменты",
+            "аксессуары",
+        ],
+    },
+});
+
+Vue.component("tabsCategories", {
+    template: `
+        <div class="tabs__wrapper">
+            <div class="tabs products__tabs mobile-overflow-slider">
+                <a v-for="(element, index) in сategories" class="products__tab tab tab--active" href="#pr-tab-{{ index + 1 }}">{{ element }}</a>
+            </div>
+         </div>
+        `,
+    data: {
+        сategories: [
+            "запчасти",
+            "моторы",
+            "шины",
+            "электроник",
+            "инструменты",
+            "аксессуары",
+        ],
+    },
+});
